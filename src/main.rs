cd10a0c25bac67b03757644473dd3e8ba02fc378
David Gardiner <david@gardiner.net.au>
2025-10-15 13:53:47 +1030

    Return JSON

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::serde::{Serialize, json::Json};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct HelloResponse {
+    message: String,
+    name: String,
 }
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/hello/<name>")]
+fn hello(name: &str) -> Json<HelloResponse> {
+    Json(HelloResponse {
+        message: format!("{}, now with extra Iron Oxide!", name),
+        name: name.to_string(),
+    })
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, hello])
+}
+
